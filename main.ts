8422e8314a5d5fe612c0a0497686e3d241521d31
maowu <mao@mutienliao.com>
2018-01-11 16:19:06 +0900

    fix block

--- main.ts
+++ main.ts
@@ -69,6 +69,7 @@ namespace GameConsole{
     //% blockGap=20 weight=75
     export function ConsoleExcue(): void {
         serial.writeLine("pitch=" + input.rotation(Rotation.Pitch))
+        serial.writeLine("roll=" + input.rotation(Rotation.Roll))
     }
 
 }
