5e27d36ac770e9255e320351e724c4d47ec2bcc8
maowu <mao@mutienliao.com>
2018-01-11 14:50:22 +0900

    fix block

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ namespace GameConsole{
     /**
     * 搖桿的執行功能，捕捉A/B按鍵以及加速計的X軸，P0/P1做換裝的設定，震動送出換裝資訊
     */
-    //% blockId="ConsoleExcue" block=""
+    //% blockId="ConsoleExcue" block="console excue"
     //% blockGap=2 weight=1
     export function ConsoleExcue(length: number, width:number): void {
         if (settingmod == 0) {
